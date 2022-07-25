c455be5e6c4e560867a83d0b66984e7de498a7be
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-25 16:50:42 +0900

    2022-07-25 CodingBat 문제 풀이 연습

---
+++ src/main/java/warmup_1/SumDouble.java
@@ -0,0 +1,17 @@
+package warmup_1;
+
+public class SumDouble {
+
+//    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+    public int sumDouble(int a, int b) {
+
+        if (a == b) {
+            return (a + b) * 2;
+        } else {
+            return a + b;
+        }
+
+    }
+
+}
